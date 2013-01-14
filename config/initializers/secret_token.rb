052db8437a669ef315222c8d984562092d278e24
codebiff <dave@codebiff.com>
2013-01-14 21:52:20 +0000

    sublime project files should be hidden

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chloroform::Application.config.secret_token = 'dae7b633f9dba18dc4ae79efcc557cad4ee9b923ce77e5bcdd2d605ed3b1218dc8387e18839bb51ab7619854660f45861a491752c0e91bdafec269839da587a3'
