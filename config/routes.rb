c6227b4353c6e9f585ea98dbfce47558c01e1a09
codebiff <dave@codebiff.com>
2013-01-15 11:16:01 +0000

    setup home and dashboard controllers. set root to home#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Chloroform::Application.routes.draw do
+  
+  root to: "home#index"
+
   devise_for :users
+
+  get "dashboard/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
