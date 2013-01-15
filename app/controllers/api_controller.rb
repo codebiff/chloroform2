feef750544e4ff30ad0b1ec40a1e0ee6b87dab05
codebiff <dave@codebiff.com>
2013-01-15 14:48:26 +0000

    find correct user from api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,9 +1,13 @@
 class ApiController < ApplicationController
 
   def submit
-    @user = User.find_by_api_key(params[:api_key])
-    Message.generate(@user, params)
-    redirect_to root_path
+    if @user = User.find_by_api_key(params[:api_key])
+      if m = Message.generate(@user, params)
+        redirect_to root_path
+      end
+    else
+      redirect_to root_path
+    end
   end
 
 end
--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe ApiController do
 
+  before :each do
+    request.env["HTTP_REFERER"] = "http://test.com"
+  end
+
   let(:user) { FactoryGirl.create(:user) }
   let(:params) { {name: "Joe Example", email: "joe@example.com", comment: "This is a short comment"} }
 
@@ -10,8 +14,19 @@ describe ApiController do
     assigns(:user).should eq(user)
   end
 
-  it "should submit a new message" do
+  it "should submit a new message if api_key matches" do
     expect{ post :submit, params.merge({api_key: user.api_key}) }.to change(user.messages, :count).by(1)
   end
+
+  it "should not find a user api_key matches" do
+    post :submit, params.merge({api_key: "foobar"})
+    assigns(:user).should be_false
+  end
+
+  it "should not submit a new message if api_key matches" do
+    expect{ post :submit, params.merge({api_key: "foobar"}) }.to change(user.messages, :count).by(0)
+  end
+
+  it "should redirect to the correct referer"
 
 end
