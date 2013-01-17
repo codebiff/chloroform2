527b6d74b1fcc94af425a709ae9c371ea9910972
codebiff <dave@codebiff.com>
2013-01-17 22:16:45 +0000

    set user messages and labels in controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,8 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @messages = current_user.messages
+    @labels = @messages.map{|m| {m.label["name"] => m.label["slug"]} }.uniq
   end
 
 end
--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -20,6 +20,30 @@ describe DashboardController do
       response.should be_ok
     end
 
+    it "should set the messages variable if empty" do
+      user.confirm!
+      sign_in(user)
+      get :index
+      assigns(:messages).should be_empty
+    end
+
+    it "should set the messages variable if messages exist" do
+      user.confirm!
+      5.times { FactoryGirl.create(:message) }
+      sign_in(user)
+      get :index
+      assigns(:messages).should eq(user.messages)
+    end
+
+    it "should generate a list of form labels" do
+      user.confirm!
+      10.times { FactoryGirl.create(:message) }
+      sign_in(user)
+      get :index
+      labels = user.messages.all.map{|m| {m.label["name"] => m.label["slug"]} }.uniq
+      assigns(:labels).should eq(labels)
+    end 
+
   end
 
 end
