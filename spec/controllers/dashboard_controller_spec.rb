d0251c96bc0e175da7e0fe6da059d18906bd8a1d
codebiff <dave@codebiff.com>
2013-01-17 19:43:08 +0000

    testing of dashboard

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,12 +1,25 @@
 require 'spec_helper'
 
 describe DashboardController do
+  include Devise::TestHelpers
+
+  let(:user) { FactoryGirl.create(:user) }
 
   describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+    
+    it "should redirecct to index if not logged in" do
+      sign_out(user)  
+      get :index
+      response.should be_redirect
     end
+
+    it "should find index if logged in" do
+      user.confirm!
+      sign_in(user)
+      get :index
+      response.should be_ok
+    end
+
   end
 
 end
